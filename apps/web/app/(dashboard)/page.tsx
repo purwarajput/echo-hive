2bab3ea2c58c9ec4a3aaf1607c6e8ebb7c62b107
Purwa <s.purwarajput@gmail.com>
2025-08-13 00:33:13 +0530

    organization

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { SignInButton, UserButton } from "@clerk/nextjs";
-import { useMutation, useQuery , Authenticated, Unauthenticated } from "convex/react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
@@ -10,21 +10,14 @@ export default function Page() {
   const addUser = useMutation(api.users.add);
 
   return (
-    <>
-    <Authenticated>
     <div className="flex flex-col items-center justify-center min-h-svh">
         <p>apps/web</p>
         <UserButton/>
+        <OrganizationSwitcher hidePersonal />
         <Button onClick={() => addUser()}>Add</Button>
         <div className="max-s-sm w-full mx-auto">
         {JSON.stringify(users, null, 2)}
         </div>
       </div>
-    </Authenticated>
-    <Unauthenticated>
-      <p>Must be signed in!</p>
-      <SignInButton>Sign in!</SignInButton>
-    </Unauthenticated>
-    </>
   )
 }
